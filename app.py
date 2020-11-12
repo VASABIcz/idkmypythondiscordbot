5a76372d07a7f56106d3ea8bd1a295f4e34ce047
VASABIcz <71564425+VASABIcz@users.noreply.github.com>
2020-11-12 11:15:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -362,4 +362,4 @@ async def on_ready():
     print('------')
 
 
-bot.run('Nzc2MjAxMzc4MzAwNjkwNDUy.X6xb3Q.kgAJjt3Ps_PqiNbjFrox29zuMes!!!'.replace("!",""))
+bot.run('Njk1MjY5OTE3NjcwMjQ0Mzk0.XoXukQ.kJjlz9boR15ZbbASQprhTjIkcOg!!!'.replace("!",""))
