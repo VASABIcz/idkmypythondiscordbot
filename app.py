144a80689cc34002516c81e06495d8a79bc5e2ba
VASABIcz <71564425+VASABIcz@users.noreply.github.com>
2021-01-15 19:04:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from os import listdir
 from discord_webhook import DiscordWebhook
 from constant import *
+
+# TODO Checks guild only ...
+#
+# known bugs
+# ¯\_(ツ)_/¯
+# TODO per guild prefix
+
 
 
 load_dotenv('.env')
@@ -12,14 +20,14 @@ TOKEN = os.environ['TOKEN']
 bot = commands.Bot(command_prefix=PREF)
 
 
-@bot.command()
+@bot.command(brief="reload cogs")
 @commands.is_owner()
 async def reload(ctx, name):
     bot.reload_extension(f'cogs.{name}')
     await ctx.channel.send('cog reloaded')
 
 
-@bot.command()
+@bot.command(brief="simple eval")
 @commands.is_owner()
 async def eval(ctx, *, eve):
     evee = (str(eval(eve)))
@@ -32,14 +40,12 @@ async def ping(ctx):
     await ctx.send(f'Pong! {bot.latency}')
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    webhook = DiscordWebhook(
-        url='https://discord.com/api/webhooks/797142718777262121/ciIaNQ-hBMIS9ZGbCCpPoDSTT1lvKQukYy4RIJTtxKB3Ue9k_RIvh-FFAR1sUKk6ooaV',
-        content=f'{ctx.author} \n {ctx.guild} \n {str(error)}')
-    response = webhook.execute()
-
-
+#@bot.event
+#async def on_command_error(ctx, error):
+#    webhook = DiscordWebhook(
+#        url='https://discord.com/api/webhooks/797142718777262121/ciIaNQ-hBMIS9ZGbCCpPoDSTT1lvKQukYy4RIJTtxKB3Ue9k_RIvh-FFAR1sUKk6ooaV',
+#        content=f'{ctx.author} \n {ctx.guild} \n {str(error)}')
+#    response = webhook.execute()
 
 
 ###NAPISE KDYZ JE BOT PRIPRAVEN K POUZIVANI
@@ -51,10 +57,11 @@ async def on_ready():
     print('---------------------')
     activity = discord.Game(name=f"{PREF}help")
     await bot.change_presence(status=discord.Status.online, activity=activity)
-    bot.load_extension('cogs.music')
-
-
-
+    files = listdir('C:\pythonProject\Discord_bot\COG_BOT\cogs')
+    for n in files:
+        if n != '__pycache__':
+            if not '#' in n:
+                bot.load_extension(f'cogs.{n.replace(".py", "")}')
 
 
 ###DISCORD BOT TOKEN
