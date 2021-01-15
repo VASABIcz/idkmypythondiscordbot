0399efe662956b8b0e3ebc0f54955c0a6c3bcc3d
VASABIcz <71564425+VASABIcz@users.noreply.github.com>
2021-01-15 19:09:06 +0100

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,1 +1,13 @@
 loljs = {}
+YDL_OPTIONS = {'format': 'bestaudio/best',
+               'restrictfilenames': True,
+               'noplaylist': True,
+               'nocheckcertificate': True,
+               'ignoreerrors': True,
+               'logtostderr': False,
+               'quiet': True,
+               'no_warnings': False,
+               'default_search': 'auto',
+               'source_address': '0.0.0.0'}
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                              'options': '-vn'}
