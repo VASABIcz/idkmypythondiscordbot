c21412722d88b16b1d9e32db8ed3d3261bad8c9a
VASABIcz <71564425+VASABIcz@users.noreply.github.com>
2021-01-28 17:40:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-import discord
-from discord.ext import commands
 import os
 from dotenv import load_dotenv
 from os import listdir
 from constant import *
+import ast
+import discord
+import io
+from discord import File
+from discord.ext import commands
 
+
+def insert_returns(body):
+    # insert return stmt if the last expression is a expression statement
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    # for if statements, we insert returns into the body and the orelse
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
+os.environ['PYTHONASYNCIODEBUG'] = '1'
+#logging.basicConfig(level=logging.DEBUG)
+#warnings.resetwarnings()
 # TODO Checks guild only ...
 #
 # known bugs
@@ -12,12 +33,10 @@ from constant import *
 # TODO per guild prefix
 
 
-
 load_dotenv('.env')
 PREF = os.environ['PREF']
 TOKEN = os.environ['TOKEN']
 bot = commands.Bot(command_prefix=PREF)
-
 
 @bot.command(brief="reload cogs")
 @commands.is_owner()
@@ -25,13 +44,75 @@ async def reload(ctx, name):
     bot.reload_extension(f'cogs.{name}')
     await ctx.channel.send('cog reloaded')
 
+@bot.command(brief="load cogs")
+@commands.is_owner()
+async def load(ctx, name):
+    bot.load_extension(f'cogs.{name}')
+
 
 @bot.command(brief="simple eval", name='eval')
 @commands.is_owner()
-async def evale(ctx, *, eve):
-    evee = (str(eval(eve)))
-    await ctx.channel.send(evee)
+async def _eval(ctx, *, cmd):
 
+    """Evaluates input.
+    Input is interpreted as newline seperated statements.
+    If the last statement is an expression, that is the return value.
+    Usable globals:
+      - `bot`: the bot instance
+      - `discord`: the discord module
+      - `commands`: the discord.ext.commands module
+      - `ctx`: the invokation context
+      - `__import__`: the builtin `__import__` function
+    Such that `>eval 1 + 1` gives `2` as the result.
+    The following invokation will cause the bot to send the text '9'
+    to the channel of invokation and return '3' as the result of evaluating
+    >eval ```
+    a = 1 + 2
+    b = a * 2
+    await ctx.send(a + b)
+    a
+    ```
+    """
+    fn_name = "_eval_expr"
+    cmd = cmd.strip("` ")
+    # add a layer of indentation
+    cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+    # wrap in async def body
+    body = f"async def {fn_name}():\n{cmd}"
+    parsed = ast.parse(body)
+    body = parsed.body[0].body
+    insert_returns(body)
+    env = {
+        'bot': ctx.bot,
+        'discord': discord,
+        'commands': commands,
+        'ctx': ctx,
+        '__import__': __import__,
+        'loljs': loljs,
+        'FFMPEG_OPTIONS': FFMPEG_OPTIONS
+    }
+    exec(compile(parsed, filename="<ast>", mode="exec"), env)
+    result = (await eval(f"{fn_name}()", env))
+    f = io.StringIO(str(result))
+    await ctx.channel.send(content="yep", file=File(fp=f, filename="output.txt"))
+    try:
+        await ctx.send(result)
+    except:
+        pass
+
+@bot.command()
+async def covert(ctx, *, cmd):
+    cmd = str(cmd)
+    cmd = cmd.replace("print", "await ctx.channel.send")
+    cmd = cmd.replace("input", "await bot.wait_for('message')")
+    await ctx.send(cmd)
+
+@bot.command()
+async def me(ctx, mem):
+    for i in ctx.guild.emojis:
+        if str(mem) in str(i):
+            i = str(i)
+            await ctx.channel.send(f"i")
 
 
 @bot.command()
@@ -47,7 +128,9 @@ async def ping(ctx):
 #    response = webhook.execute()
 
 
-###NAPISE KDYZ JE BOT PRIPRAVEN K POUZIVANI
+# ##NAPISE KDYZ JE BOT PRIPRAVEN K POUZIVANI
+
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -59,9 +142,9 @@ async def on_ready():
     files = listdir('cogs')
     for n in files:
         if n != '__pycache__':
-            if not '#' in n:
+            if '#' not in n:
                 bot.load_extension(f'cogs.{n.replace(".py", "")}')
 
 
-###DISCORD BOT TOKEN
+# ##DISCORD BOT TOKEN
 bot.run(TOKEN)
